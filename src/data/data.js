14958dad841588a08f2189f7f34133f4179924fc
“Maja” <mawenjadelin@gmail.com>
2026-06-03 15:48:35 +0200

    made it possible to generate page from json data

--- src/data/data.js
+++ src/data/data.js
@@ -15,8 +15,10 @@ export const data = {
           type: "media-left",
           intro: `<p>Intra-action signifies the mutual constitution of entangled agencies. That is, in contrast to the usual ‘interaction,’ which assumes the prior existence of independent entities, intra-action recognizes that distinct agencies emerge through their intra-action.</p>`,
           title: "Karen Barad",
-          subtext:
-            "Barad, K. (2007). Meeting the universe halfway: Quantum physics and the entanglement of matter and meaning. duke university Press. p. 141",
+          subtext: {
+            type: "source",
+            text: "Barad, K. (2007). Meeting the universe halfway: Quantum physics and the entanglement of matter and meaning. duke university Press. p. 141"
+          },
           media: [
             {
               type: "image",
@@ -25,36 +27,32 @@ export const data = {
             },
           ],
         },
+        {
+          type: "media-left",
+          intro: `<h2>Immaterials: Light Painting Wifi</h2><p>The project explores the invisible terrain of WiFi networks in urban spaces by light painting signal strength in long-exposure photographs. A four-metre tall measuring rod with 80 points of light reveals cross-sections through WiFi networks using a photographic technique called light-painting.</p>`,
+          title: "Timo Arnall",
+          subtext: {
+            type: "link",
+            text: "https://www.elasticspace.com/2013/09/the-immaterials-project"
+          },
+          media: [
+            {
+              type: "image",
+              src: "/public/assets/images/intra-action-1.png",
+              alt: "Wifi portrayed as light point on a wall that together form a landscape like shape."
+            },
+          ]
+        }
       ],
-
-      // {
-      //     section-1: {
-      //         className: "photo-left",
-      //         text: "Intra-action signifies the mutual constitution of entangled agencies. That is, in contrast to the usual ‘interaction,’ which assumes the prior existence of independent entities, intra-action recognizes that distinct agencies emerge through their intra-action.",
-      //         title: "Karen Barad",
-      //         link: "Barad, K. (2007). Meeting the universe halfway: Quantum physics and the entanglement of matter and meaning. duke university Press. p. 141",
-      //         images: {
-      //             image-1: {
-      //                 source: "/public/assets/images/intra-action-2.png",
-      //                 alt: "the word intra-action written several times, overlapping in a way that makes it almost unreadable."
-      //             }
-      //         }
-      //     },
-      //     section-2: {
-      //         className: "photo-left",
-      //         heading-2: "Immaterials: Light Painting Wifi",
-      //         text: "The project explores the invisible terrain of WiFi networks in urban spaces by light painting signal strength in long-exposure photographs. A four-metre tall measuring rod with 80 points of light reveals cross-sections through WiFi networks using a photographic technique called light-painting.",
-      //         author: "Timo Arnall",
-      //         link: "https://www.elasticspace.com/2013/09/the-immaterials-project",
-      //         images: {
-      //             image-1: {
-      //                 source: "/public/assets/images/intra-action-1.png",
-      //                 alt: "Wifi portrayed as light point on a wall that together form a landscape like shape."
-      //             }
-      //         }
-      //     },
-      //     general-image: "/public/assets/images/intra-action-3.png"
-      // }
+      generalImage: {
+        type: "image",
+        src: "/public/assets/images/intra-action-3.png"
+      }
     },
+    {
+      title: "example",
+      slug: "example",
+      categories: ["example-category"],
+    }
   ],
 };
