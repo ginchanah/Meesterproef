d519fbca7df8f7bb133b249c835184bc73901348
“Maja” <mawenjadelin@gmail.com>
2026-06-09 14:51:46 +0200

    Merge branch 'dev' into feature/footer

--- src/data/data.js
+++ src/data/data.js
@@ -15,17 +15,17 @@ export const data = {
           type: "media-left",
           intro: {
             type: "quote",
-            text: "Intra-action signifies the mutual constitution of entangled agencies. That is, in contrast to the usual ‘interaction,’ which assumes the prior existence of independent entities, intra-action recognizes that distinct agencies emerge through their intra-action."
+            text: `<b>Intra-action</b> signifies the mutual constitution of entangled agencies. That is, in contrast to the usual ‘interaction,’ which assumes the prior existence of independent entities, intra-action recognizes that distinct agencies emerge through their intra-action.`
           },
           author: "Karen Barad",
           subtext: {
             type: "source",
-            text: "Barad, K. (2007). Meeting the universe halfway: Quantum physics and the entanglement of matter and meaning. duke university Press. p. 141"
+            text: `Barad, K. (2007). Meeting the universe halfway: Quantum physics and the entanglement of matter and meaning. duke university Press. p. 141`
           },
           media: [
             {
               type: "image",
-              src: "/public/assets/images/intra-action-2.png",
+              src: "/public/assets/media/intra-action-2.png",
               alt: "The word intra-action written several times, overlapping in a way that makes it almost unreadable.",
             },
           ],
@@ -35,17 +35,17 @@ export const data = {
           heading: "Immaterials: Light Painting Wifi",
           intro: {
             type: "text",
-            text: "The project explores the invisible terrain of WiFi networks in urban spaces by light painting signal strength in long-exposure photographs. A four-metre tall measuring rod with 80 points of light reveals cross-sections through WiFi networks using a photographic technique called light-painting."
+            text: `The project explores the invisible terrain of WiFi networks in urban spaces by light painting signal strength in long-exposure photographs. A four-metre tall measuring rod with 80 points of light reveals cross-sections through WiFi networks using a photographic technique called light-painting.`
           },
           author: "Timo Arnall",
           subtext: {
             type: "link",
-            text: "https://www.elasticspace.com/2013/09/the-immaterials-project"
+            text: `https://www.elasticspace.com/2013/09/the-immaterials-project`
           },
           media: [
             {
               type: "image",
-              src: "/public/assets/images/intra-action-1.png",
+              src: "/public/assets/media/intra-action-1.png",
               alt: "Wifi portrayed as light point on a wall that together form a landscape like shape."
             },
           ]
@@ -53,13 +53,172 @@ export const data = {
       ],
       generalImage: {
         type: "image",
-        src: "/public/assets/images/intra-action-3.png"
+        src: "/public/assets/media/intra-action-3.png"
+      }
+    },
+    {
+      title: "Inter-action",
+      slug: "example",
+      categories: ["example-category"],
+    },
+    {
+      title: "Treshold",
+      slug: "example",
+      categories: ["example-category"],
+    },
+    {
+      title: "System",
+      slug: "example",
+      categories: ["example-category3"],
+    },
+    {
+      title: "Adaptation",
+      slug: "adaptation",
+      categories: ["example-category3"],
+    },
+    {
+      title: "Balancing Feedback",
+      slug: "balancing-feedback",
+      categories: ["example-category3"],
+    },
+    {
+      title: "Reinforcing Feedback",
+      slug: "reinforcing-feedback",
+      categories: ["example-category3"],
+      content: [
+        {
+          type: "media-left",
+          intro: {
+            type: "quote",
+            text: `Reinforcing or positive feedback loops are self-enhancing, leading to exponential growth or to runaway collapses over time.`
+          },
+          author: "",
+          subtext: {
+            type: "source",
+            text: `Meadows, D. (2008). Thinking in systems: International bestseller. Chelsea green publishing.`
+          },
+          media: [
+            {
+              type: "image",
+              src: "/public/assets/media/reinforcing-feedback-visualisation.png",
+              alt: "The story from the artwork repeated multiple times vertically, the further you go down the more it is repeated and overlapped making the sentences in the end impossible to read."
+            }
+          ],
+        },
+        {
+          type: "media-right",
+          heading: "I'm Sitting in a Room by Alvin Lucier",
+          intro: {
+            type: "text",
+            text: `I'm Sitting in a Room by Alvin Lucier is a sound piece where the artist records himself reading a text, then repeatedly replays and re-records it in the same room. Over time, his speech dissolves, from intelligibility, leaving only the resonant frequencies of the space - the room itself becomes the music.`
+          },
+          author: "I'm sitting in a room (1969). Alvin Lucier, performance",
+          subtext: {
+            type: "link",
+            text: `https://www.youtube.com/watch?v=bhtO4DsSazc`
+          },
+          media: [
+            {
+              type: "image",
+              src: "/public/assets/media/reinforcing-feedback-artwork.png",
+              alt: "Alvin Lucier sitting on a chair in a room with big white stairs behind him. He is hooked up to a recording device and is wearing a brown suit with a red tie."
+            },
+          ]
+        }
+      ],
+      generalImage: {
+        type: "image",
+        src: "/public/assets/media/reinforcing-feedback-title.png"
+      }
+    },
+    {
+      title: "Invisible",
+      slug: "example",
+      categories: ["example-category"],
+    },
+    {
+      title: "Interface",
+      slug: "example",
+      categories: ["example-category2"],
+    },
+    {
+      title: "Ma (in-between)",
+      slug: "ma",
+      categories: ["category-1"],
+      content: [
+        {
+          type: "media-left",
+          intro: {
+            type: "quote",
+            text: `The Japanese concept of ma (間) assigns subjecthood to the tension between two entities. It suggests that the subject exists not in the human or the external world but in a third dimension - the dialogue between them. The in-between does not distinguish between humans, non-humans, nature, and man-made objects; rather, it represents a new nature of intelligence that emarges in the space between.`
+          },
+          author: "",
+          subtext: {
+            type: "source",
+            text: `Venice Biennale Series- Biennale Architettura 2025 Intelligens. Natural. Artificial. Collective. Japanese Pavillion`
+          },
+          media: [
+            {
+              type: "image",
+              src: "/public/assets/media/ma-visualisation.png",
+              alt: "ma written out in a frame. The oudside of part both letters is cut off."
+            },
+          ],
+        },
+        {
+          type: "media-right",
+          heading: "Skyspace",
+          intro: {
+            type: "text",
+            text: `In Skyspace, James Turrell creates an architectural aperture where sky, light, and time merge in a single perceptual experience. A square or circular opening in the ceiling frames the sky, not as an object to observe, but as a presence that continuously shifts. Inside, silence prevails, allowing the viewer’s act of seeing itself to become the focus.`
+          },
+          author: "James Turrell, skyspaces (1974 - now)",
+          subtext: {
+            type: "link",
+            text: `https://www.youtube.com/watch?v=_BuJpDXkMz8`
+          },
+          media: [
+            {
+              type: "image",
+              src: "/public/assets/media/ma-artwork.png",
+              alt: "Picture of the ceiling of a white room. The ceiling is square and in the middle there is a square window/hole. Trough it you can view a blue sky. It is the artwork skyspaces from James Turrell. Where he plays with light."
+            },
+          ]
+        },
+        {
+          type: "media-left",
+          heading: "Helvetica",
+          intro: {
+            type: "intro",
+            text: `Massimo Vignelli talking about the space between the characters that makes it, in Helvetica, a documentary about the typeface Helvertica, directed by Gary`
+          },
+          subtext: {
+            type: "link",
+            text: `https://www.youtube.com/watch?v=9g3Ogtgleyg`
+          },
+          media: [
+            {
+              type: "image",
+              src:  "/public/assets/media/ma-title.png",
+              alt:  "ma written out in a frame. The oudside part of both letters is cut off."
+            },
+          ]
+        }
+      ],
+      generalImage: {
+        type: "image",
+        src: "/public/assets/media/ma-title.png"
       }
     },
     {
       title: "example",
       slug: "example",
-      categories: ["example-category"],
-    }
+      categories: ["example-category2"],
+    },
+    {
+      title: "Lines of Flight",
+      slug: "lines-of-flight",
+      categories: ["example-category2"],
+    },
   ],
 };
