48cc999d3ead12f30578c1b749836a93ea89142a
“Maja” <mawenjadelin@gmail.com>
2026-06-12 13:20:47 +0200

    added data interface

--- src/data/data.js
+++ src/data/data.js
@@ -4,23 +4,20 @@ export const data = {
       title: "Intra-action",
       slug: "intra-action",
       categories: ["category-1"],
-      titleAnimation: {
-        numberOfTexts: 20,
-        repeatTextBlocks: 3,
-        word: "intra-action",
-        className: "intra-action"
-      },
       content: [
         {
           type: "media-left",
-          intro: {
-            type: "quote",
-            text: `<b>Intra-action</b> signifies the mutual constitution of entangled agencies. That is, in contrast to the usual ‘interaction,’ which assumes the prior existence of independent entities, intra-action recognizes that distinct agencies emerge through their intra-action.`
-          },
+          heading: "",
+          intro: [
+            {
+              type: "quote",
+              text: `<b>Intra-action</b> signifies the mutual constitution of entangled agencies. That is, in contrast to the usual ‘interaction,’ which assumes the prior existence of independent entities, intra-action recognizes that distinct agencies emerge through their intra-action.`,
+            },
+          ],
           author: "Karen Barad",
           subtext: {
             type: "source",
-            text: `Barad, K. (2007). Meeting the universe halfway: Quantum physics and the entanglement of matter and meaning. duke university Press. p. 141`
+            text: `Barad, K. (2007). Meeting the universe halfway: Quantum physics and the entanglement of matter and meaning. duke university Press. p. 141`,
           },
           media: [
             {
@@ -33,28 +30,30 @@ export const data = {
         {
           type: "media-right",
           heading: "Immaterials: Light Painting Wifi",
-          intro: {
-            type: "text",
-            text: `The project explores the invisible terrain of WiFi networks in urban spaces by light painting signal strength in long-exposure photographs. A four-metre tall measuring rod with 80 points of light reveals cross-sections through WiFi networks using a photographic technique called light-painting.`
-          },
+          intro: [
+            {
+              type: "text",
+              text: `The project explores the invisible terrain of WiFi networks in urban spaces by light painting signal strength in long-exposure photographs. A four-metre tall measuring rod with 80 points of light reveals cross-sections through WiFi networks using a photographic technique called light-painting.`,
+            },
+          ],
           author: "Timo Arnall",
           subtext: {
             type: "link",
-            text: `https://www.elasticspace.com/2013/09/the-immaterials-project`
+            text: `https://www.elasticspace.com/2013/09/the-immaterials-project`,
           },
           media: [
             {
               type: "image",
               src: "/assets/media/intra-action-1.png",
-              alt: "Wifi portrayed as light point on a wall that together form a landscape like shape."
+              alt: "Wifi portrayed as light point on a wall that together form a landscape like shape.",
             },
-          ]
-        }
+          ],
+        },
       ],
       generalImage: {
         type: "image",
-        src: "/assets/media/intra-action-3.png"
-      }
+        src: "/assets/media/intra-action-3.png",
+      },
     },
     {
       title: "Adaptation",
@@ -73,48 +72,53 @@ export const data = {
       content: [
         {
           type: "media-left",
-          intro: {
-            type: "quote",
-            text: `Reinforcing or positive feedback loops are self-enhancing, leading to exponential growth or to runaway collapses over time.`
-          },
+          heading: "",
+          intro: [
+            {
+              type: "quote",
+              text: `Reinforcing or positive feedback loops are self-enhancing, leading to exponential growth or to runaway collapses over time.`,
+            },
+          ],
           author: "",
           subtext: {
             type: "source",
-            text: `Meadows, D. (2008). Thinking in systems: International bestseller. Chelsea green publishing.`
+            text: `Meadows, D. (2008). Thinking in systems: International bestseller. Chelsea green publishing.`,
           },
           media: [
             {
               type: "image",
               src: "/assets/media/reinforcing-feedback-visualisation.png",
-              alt: "The story from the artwork repeated multiple times vertically, the further you go down the more it is repeated and overlapped making the sentences in the end impossible to read."
-            }
+              alt: "The story from the artwork repeated multiple times vertically, the further you go down the more it is repeated and overlapped making the sentences in the end impossible to read.",
+            },
           ],
         },
         {
           type: "media-right",
           heading: "I'm Sitting in a Room by Alvin Lucier",
-          intro: {
-            type: "text",
-            text: `I'm Sitting in a Room by Alvin Lucier is a sound piece where the artist records himself reading a text, then repeatedly replays and re-records it in the same room. Over time, his speech dissolves, from intelligibility, leaving only the resonant frequencies of the space - the room itself becomes the music.`
-          },
+          intro: [
+            {
+              type: "text",
+              text: `I'm Sitting in a Room by Alvin Lucier is a sound piece where the artist records himself reading a text, then repeatedly replays and re-records it in the same room. Over time, his speech dissolves, from intelligibility, leaving only the resonant frequencies of the space - the room itself becomes the music.`,
+            },
+          ],
           author: "I'm sitting in a room (1969). Alvin Lucier, performance",
           subtext: {
             type: "link",
-            text: `https://www.youtube.com/watch?v=bhtO4DsSazc`
+            text: `https://www.youtube.com/watch?v=bhtO4DsSazc`,
           },
           media: [
             {
               type: "image",
               src: "/assets/media/reinforcing-feedback-artwork.png",
-              alt: "Alvin Lucier sitting on a chair in a room with big white stairs behind him. He is hooked up to a recording device and is wearing a brown suit with a red tie."
+              alt: "Alvin Lucier sitting on a chair in a room with big white stairs behind him. He is hooked up to a recording device and is wearing a brown suit with a red tie.",
             },
-          ]
-        }
+          ],
+        },
       ],
       generalImage: {
         type: "image",
-        src: "/assets/media/reinforcing-feedback-title.png"
-      }
+        src: "/assets/media/reinforcing-feedback-title.png",
+      },
     },
     {
       title: "Ma (in-between)",
@@ -123,72 +127,138 @@ export const data = {
       content: [
         {
           type: "media-left",
-          intro: {
-            type: "quote",
-            text: `The Japanese concept of ma (間) assigns subjecthood to the tension between two entities. It suggests that the subject exists not in the human or the external world but in a third dimension - the dialogue between them. The in-between does not distinguish between humans, non-humans, nature, and man-made objects; rather, it represents a new nature of intelligence that emarges in the space between.`
-          },
+          heading: "",
+          intro: [
+            {
+              type: "quote",
+              text: `The Japanese concept of ma (間) assigns subjecthood to the tension between two entities. It suggests that the subject exists not in the human or the external world but in a third dimension - the dialogue between them. The in-between does not distinguish between humans, non-humans, nature, and man-made objects; rather, it represents a new nature of intelligence that emarges in the space between.`,
+            },
+          ],
           author: "",
           subtext: {
             type: "source",
-            text: `Venice Biennale Series- Biennale Architettura 2025 Intelligens. Natural. Artificial. Collective. Japanese Pavillion`
+            text: `Venice Biennale Series- Biennale Architettura 2025 Intelligens. Natural. Artificial. Collective. Japanese Pavillion`,
           },
           media: [
             {
               type: "image",
               src: "/assets/media/ma-visualisation.png",
-              alt: "ma written out in a frame. The oudside of part both letters is cut off."
+              alt: "ma written out in a frame. The oudside of part both letters is cut off.",
             },
           ],
         },
         {
           type: "media-right",
           heading: "Skyspace",
-          intro: {
-            type: "text",
-            text: `In Skyspace, James Turrell creates an architectural aperture where sky, light, and time merge in a single perceptual experience. A square or circular opening in the ceiling frames the sky, not as an object to observe, but as a presence that continuously shifts. Inside, silence prevails, allowing the viewer’s act of seeing itself to become the focus.`
-          },
+          intro: [
+            {
+              type: "text",
+              text: `In Skyspace, James Turrell creates an architectural aperture where sky, light, and time merge in a single perceptual experience. A square or circular opening in the ceiling frames the sky, not as an object to observe, but as a presence that continuously shifts. Inside, silence prevails, allowing the viewer’s act of seeing itself to become the focus.`,
+            },
+          ],
           author: "James Turrell, skyspaces (1974 - now)",
           subtext: {
             type: "link",
-            text: `https://www.youtube.com/watch?v=_BuJpDXkMz8`
+            text: `https://www.youtube.com/watch?v=_BuJpDXkMz8`,
           },
           media: [
             {
               type: "image",
               src: "/assets/media/ma-artwork.png",
-              alt: "Picture of the ceiling of a white room. The ceiling is square and in the middle there is a square window/hole. Trough it you can view a blue sky. It is the artwork skyspaces from James Turrell. Where he plays with light."
+              alt: "Picture of the ceiling of a white room. The ceiling is square and in the middle there is a square window/hole. Trough it you can view a blue sky. It is the artwork skyspaces from James Turrell. Where he plays with light.",
             },
-          ]
+          ],
         },
         {
           type: "media-left",
           heading: "Helvetica",
-          intro: {
-            type: "text",
-            text: `Massimo Vignelli talking about the space between the characters that makes it, in Helvetica, a documentary about the typeface Helvertica, directed by Gary`
-          },
+          intro: [
+            {
+              type: "text",
+              text: `Massimo Vignelli talking about the space between the characters that makes it, in Helvetica, a documentary about the typeface Helvertica, directed by Gary`,
+            },
+          ],
           subtext: {
             type: "link",
-            text: `https://www.youtube.com/watch?v=9g3Ogtgleyg`
+            text: `https://www.youtube.com/watch?v=9g3Ogtgleyg`,
           },
           media: [
             {
               type: "image",
               src: "/assets/media/ma-title.png",
-              alt: "ma written out in a frame. The oudside part of both letters is cut off."
+              alt: "ma written out in a frame. The oudside part of both letters is cut off.",
             },
-          ]
-        }
+          ],
+        },
       ],
       generalImage: {
         type: "image",
-        src: "/assets/media/ma-title.png"
-      }
+        src: "/assets/media/ma-title.png",
+      },
     },
     {
       title: "Lines of Flight",
       slug: "lines-of-flight",
       categories: ["example-category2"],
+    },
+    {
+      title: "Interface",
+      slug: "interface",
+      categories: ["example-category3"],
+      content: [
+        {
+          type: "media-left",
+          heading: "",
+          intro: [
+            {
+              type: "quote",
+              text: `‘The <b>interface</b> constitutes the gateway through which the reservoir of human agency and experience is situated with respect to all that stands outside of it, whether technological, material, social, economic, or political.’`,
+            },
+            {
+              type: "text",
+              text: `<b>Hookway</b> sees interfaces as the crucial sites where the human and the non-human meet and influence each other, with the interface simultaneously establishing both separation and connection. It is a philosophical perspective on technology: interfaces shape not only how things function, but also how we exist and experience the world in an increasingly technological environment`,
+            },
+          ],
+          author: "",
+          subtext: {
+            type: "source",
+            text: `Hookway, B. (2014). Interface. MIT Press.`,
+          },
+          media: [
+            {
+              type: "image",
+              src: "/assets/media/interface-1.png",
+              alt: "A set of horizontal lines that come together in the center, where they turn downwards and form a line, forming the illustion of an open book.",
+            },
+          ],
+        },
+        {
+          type: "media-right",
+          heading: "Marcel Duchamp",
+          intro: [
+            {
+              type: "text",
+              text: `Perhaps one of Marcel Duchamp’s most obscure pieces is his Door, 11 rue Larrey. This was a door that that Duchamp had a carpenter construct in 1927 in the corner of a room in Duchamp’s apartment. Because of this location, the door closed one entrance when it opened the other, thereby contradicting the French proverb ‘Il faut qu’une porte soit ouverte ou fermée’ (A door must be either open or close).`,
+            },
+          ],
+          author: "",
+          subtext: {
+            type: "source",
+            text: `Door, 11, rue Larrey apartment, Paris 1927 - Marcel Duchamp`,
+          },
+          media: [
+            {
+              type: "image",
+              src: "/assets/media/interface-2.png",
+              alt: "A door that faces two open rooms on a corner, hinged in the middle, making the door always simultaniously opening and closing a room.",
+            },
+          ],
+        },
+      ],
+      generalImage: {
+        type: "image",
+        src: "/assets/media/interface-3.png",
+      },
     },
   ],
 };
